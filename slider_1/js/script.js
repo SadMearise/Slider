dee2a227086d04723ee56bbcc268eddf922b1f93
SadMearise <65180406+SadMearise@users.noreply.github.com>
2021-11-06 14:21:56 +0300

    v 1.0
    adaptive slider

---
+++ slider_1/js/script.js
@@ -0,0 +1,44 @@
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider-line');
+
+let width;
+let count = 0;
+
+function init() {
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+
+    images.forEach(item => {
+        item.style.width = width + 'px';
+    })
+    rollSlider();
+}
+
+window.addEventListener('resize', init);
+init();
+
+document.querySelector('.button-next').addEventListener('click', function() {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+    
+})
+
+document.querySelector('.button-prev').addEventListener('click', function() {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider();
+    
+})
+
+function rollSlider() {
+    sliderLine.style.transform = `translate(-${count * width}px)`;
+}
+
+
+
+
